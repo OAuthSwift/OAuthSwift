542e0a1d232836b47f3af15ad3f6d416b3131ddf
Dongri Jin <dongriat@gmail.com>
2015-11-28 18:39:04 +0900

    Remove comment

--- OAuthSwift/String+OAuthSwift.swift
+++ OAuthSwift/String+OAuthSwift.swift
@@ -71,7 +71,6 @@ extension String {
         return self.stringByRemovingPercentEncoding ?? self
     }
     
-    //分割字符
     func split(s:String)->[String]{
         if s.isEmpty{
             var x=[String]()
@@ -82,11 +81,9 @@ extension String {
         }
         return self.componentsSeparatedByString(s)
     }
-    //去掉左右空格
     func trim()->String{
         return self.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
     }
-    //是否包含字符串
     func has(s:String)->Bool{
         if (self.rangeOfString(s) != nil) {
             return true
@@ -94,7 +91,6 @@ extension String {
             return false
         }
     }
-    //是否包含前缀
     func hasBegin(s:String)->Bool{
         if self.hasPrefix(s) {
             return true
@@ -102,7 +98,6 @@ extension String {
             return false
         }
     }
-    //是否包含后缀
     func hasEnd(s:String)->Bool{
         if self.hasSuffix(s) {
             return true
@@ -110,15 +105,12 @@ extension String {
             return false
         }
     }
-    //统计长度
     func length()->Int{
         return self.utf16.count
     }
-    //统计长度(别名)
     func size()->Int{
         return self.utf16.count
     }
-    //重复字符串
     func `repeat`(times: Int) -> String{
         var result = ""
         for _ in 0..<times {
@@ -126,7 +118,6 @@ extension String {
         }
         return result
     }
-    //反转
     func reverse()-> String{
         let s=Array(self.split("").reverse())
         var x=""
