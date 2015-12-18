8f12ae1d8ab67bfa9031041466d9c828f391f357
T Kawaji <t.kawaji@gmail.com>
2015-12-19 00:21:58 +0900

    Re-add OAuthSwiftMultipartData to each target in XCode

--- OAuthSwift/OAuthSwiftMultipartData.swift
+++ OAuthSwift/OAuthSwiftMultipartData.swift
@@ -1,9 +1,9 @@
 //
 //  OAuthSwiftMultipartData.swift
-//  Pods
+//  OAuthSwift
 //
-//  Created by Tomohiro Kawaji on 2015/12/18.
-//
+//  Created by Tomohiro Kawaji on 12/18/15.
+//  Copyright (c) 2015 Dongri Jin. All rights reserved.
 //
 
 import Foundation
