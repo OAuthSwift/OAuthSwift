bfa72a0609d2ead6eccf0cc541fb9948a900b101
Paul <psalmon@abbrevi8.com>
2016-02-24 17:51:10 +1100

    Custom parameters for authorisation

--- OAuthSwift/OAuthSwiftHTTPRequest.swift
+++ OAuthSwift/OAuthSwiftHTTPRequest.swift
@@ -22,7 +22,7 @@ public class OAuthSwiftHTTPRequest: NSObject, NSURLSessionDelegate {
     }
     
     @objc public enum ParamsLocation : Int {
-        case AuthorizationHeader, /*FormEncodedBody,*/ RequestURIQuery
+        case AuthorizationHeader, /*FormEncodedBody,*/ RequestURIQuery, CustomParameters
     }
 
     var URL: NSURL
@@ -195,7 +195,7 @@ public class OAuthSwiftHTTPRequest: NSObject, NSURLSessionDelegate {
             switch (paramsLocation) {
             case .AuthorizationHeader:
                 finalParameters = parameters.filter { key, _ in !key.hasPrefix("oauth_") }
-            case .RequestURIQuery:
+            default:
                 finalParameters = parameters
             }
 
