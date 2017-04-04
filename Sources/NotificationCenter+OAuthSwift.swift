3d8a7e159e0c2dac8532db4ad6dd0287e645f295
hiragram <y@yura.me>
2017-04-04 17:29:07 +0900

    Deprecate CallbackNotification.notificationName and move to extension of Notification.Name

---
+++ Sources/NotificationCenter+OAuthSwift.swift
@@ -0,0 +1,13 @@
+//
+//  NotificationCenter+OAuthSwift.swift
+//  OAuthSwift
+//
+//  Created by hiragram on 2017/04/04.
+//  Copyright © 2017年 Dongri Jin. All rights reserved.
+//
+
+import Foundation
+
+public extension Notification.Name {
+    public static let OAuthSwiftHandleCallbackURL: Notification.Name = .init("OAuthSwiftCallbackNotificationName")
+}
