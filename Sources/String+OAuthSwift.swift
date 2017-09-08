aa85813e8468d65b6fdff910b2cd33fe3a71a0d9
Col <col.w.harris@gmail.com>
2017-09-08 11:43:04 +0800

    Remove unnecessary swiftlint comment
    It's not necessary to disable force_cast here as the code is not causing a force_cast error.

--- Sources/String+OAuthSwift.swift
+++ Sources/String+OAuthSwift.swift
@@ -102,7 +102,6 @@ extension String.Encoding {
 
     var charset: String {
         let charset = CFStringConvertEncodingToIANACharSetName(CFStringConvertNSStringEncodingToEncoding(self.rawValue))
-        // swiftlint:disable force_cast
         return charset! as String
     }
 
