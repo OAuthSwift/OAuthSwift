3ca3188badb7531c6215d4a61e97373f7317b466
teaualune <teaualune@gmail.com>
2017-11-05 16:33:25 +0800

    remove String.characters warnings

--- Sources/String+OAuthSwift.swift
+++ Sources/String+OAuthSwift.swift
@@ -46,7 +46,7 @@ extension String {
         var string = self
 
         if hasPrefix(elementSeparator) {
-            string = String(characters.dropFirst(1))
+            string = String(dropFirst(1))
         }
 
         var parameters = [String: String]()
