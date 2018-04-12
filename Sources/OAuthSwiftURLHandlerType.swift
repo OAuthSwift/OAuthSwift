5185a23a395d465b127df22811f053139cf0400e
Shai Ben-Tovim <shaiben-tovim@shais-new-mbp.home>
2018-04-12 12:21:11 +0300

    - SafariURLHandler now referencing oauthSwift as weak var (was strong let) to prevent retention cycle with OAuthSwift

--- Sources/OAuthSwiftURLHandlerType.swift
+++ Sources/OAuthSwiftURLHandlerType.swift
@@ -47,7 +47,7 @@ import SafariServices
 
         public typealias UITransion = (_ controller: SFSafariViewController, _ handler: SafariURLHandler) -> Void
 
-        open let oauthSwift: OAuthSwift
+        weak open var oauthSwift: OAuthSwift?
         open var present: UITransion
         open var dismiss: UITransion
         /// retains observers
@@ -123,7 +123,7 @@ import SafariServices
         /// Clear internal observers on authentification flow
         open func clearObservers() {
             clearLocalObservers()
-            self.oauthSwift.removeCallbackNotificationObserver()
+            self.oauthSwift?.removeCallbackNotificationObserver()
         }
 
         open func clearLocalObservers() {
