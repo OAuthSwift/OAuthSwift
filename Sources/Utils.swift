6faed68bcaedb664f4144a038d7c141064379724
Mikhail <wowivan2011@yandex.ru>
2021-03-02 09:25:49 +0300

    Added code verifier & code challenge generator for working with PKCE

--- Sources/Utils.swift
+++ Sources/Utils.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import CommonCrypto
 
 public func generateState(withLength len: Int) -> String {
     let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
@@ -20,4 +21,34 @@ public func generateState(withLength len: Int) -> String {
         randomString += String(letter)
     }
     return randomString
+}
+
+/// Generating a code verifier for PKCE
+public func generateCodeVerifier() -> String? {
+    var buffer = [UInt8](repeating: 0, count: 32)
+    _ = SecRandomCopyBytes(kSecRandomDefault, buffer.count, &buffer)
+   let codeVerifier = Data(buffer).base64EncodedString()
+        .replacingOccurrences(of: "+", with: "-")
+        .replacingOccurrences(of: "/", with: "_")
+        .replacingOccurrences(of: "=", with: "")
+        .trimmingCharacters(in: .whitespaces)
+    
+    return codeVerifier
+}
+
+/// Generating a code challenge for PKCE
+public func generateCodeChallenge(codeVerifier: String?) -> String? {
+    guard let verifier = codeVerifier, let data = verifier.data(using: .utf8) else { return nil }
+    var buffer = [UInt8](repeating: 0,  count: Int(CC_SHA256_DIGEST_LENGTH))
+    data.withUnsafeBytes {
+        _ = CC_SHA256($0.baseAddress, CC_LONG(data.count), &buffer)
+    }
+    let hash = Data(buffer)
+    let challenge = hash.base64EncodedString()
+        .replacingOccurrences(of: "+", with: "-")
+        .replacingOccurrences(of: "/", with: "_")
+        .replacingOccurrences(of: "=", with: "")
+        .trimmingCharacters(in: .whitespaces)
+   
+    return challenge
 }
